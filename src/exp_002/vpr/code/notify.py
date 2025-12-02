22efd224138e54ac067c0e215a82a48a6c08ef8f
LucaCantagallo <luca.cantagallo99@gmail.com>
2025-12-02 13:49:38 +0000

    TryGit

--- src/exp_002/vpr/code/notify.py
+++ src/exp_002/vpr/code/notify.py
@@ -11,7 +11,7 @@ def send_telegram_message(message):
     chat_id = config["telegram"]["chat_id"]
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     data = {"chat_id": chat_id, "text": message}
-    requests.post(url, data=data)
+    requests.post(url, data=data) 
 
 if __name__ == "__main__":
     send_telegram_message("Prova di notifica Telegram dal modulo notify.py")
